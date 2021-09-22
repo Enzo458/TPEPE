bac2859458db6ac65cd60e23ed7f6defa4423c72
EnzoC <enzonicolasconti@gmial.com>
2021-09-22 17:07:22 -0300

    accion notificar aucencia

--- actions/actions.py
+++ actions/actions.py
@@ -8,26 +8,13 @@
 # This is a simple example for a custom action which utters "Hello World!"
 import json
 import os
+import datetime
 from os import name
 from typing import Any, Text, Dict, List
 #
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-#
-#
-class ActionHelloWorld(Action):
-#
-     def name(self) -> Text:
-         return "action_hello_world"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-        
-        return []
-
+from rasa_sdk.events import SlotSet
 
 class ActionInfoEmpledo(Action):
 #
@@ -39,18 +26,46 @@ class ActionInfoEmpledo(Action):
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
             direccionArch= os.getcwd() +"\\recursos\\empleados\\" + next(tracker.get_latest_entity_values("idEmpleado"), None) + ".json"
             dicc= leerArchivo(direccionArch)
-            dispatcher.utter_message(dicc["nombre"])
-        
-            return []
+            if(dicc is None):
+                dispatcher.utter_message("idEmpleado incorrecto")
+                return [SlotSet("identificadorEmpleado", None)]
+            else:
+                dispatcher.utter_message(dicc)
+                return [SlotSet("identificadorEmpleado", str(dicc["idEmpleado"]))]
+            
 
+class registrarNotificacionDeAusencia(Action):
+     def name(self)-> Text:
+         return "action_registar_notificacion_falta"
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+            idEmpleado= tracker.get_slot("idEmpleado")
+            mensaje= "empleado no identificado"
+            if(idEmpleado is not None):
+                direccion= os.getcwd() +"\\recursos\\empleados\\" + next(tracker.get_latest_entity_values("idEmpleado"), None) + ".json"
+                dicc= leerArchivo(direccion)
+                date= datetime.datetime.now()
+                dicc["faltas"].append(str(date.day+"/"+date.month+"/"+date.year))
+                mensaje= "tienes " + str(len(dicc["faltas"])) +" faltas acumuladas"
+                escribirArchivo(direccion,dicc)
+            dispatcher.utter_message(mensaje)
+            return[]
 
 def leerArchivo(dire)-> dict:
-        with open (dire, "r") as archivo:
-            dict= json.load(archivo)
-            archivo.close()
-        return dict
+        try:
+            with open (dire, "r") as archivo:
+                dict= json.load(archivo)
+                archivo.close()
+            return dict
+        except FileNotFoundError:
+            return None
 
-def escribirArchivo(dire, dicc): 
-        with open(dire, "w") as archivo:
-            json.dump(dicc, archivo)
-            archivo.close()
+def escribirArchivo(dire, dicc)-> bool: 
+        try:
+            with open(dire, "w") as archivo:
+                json.dump(dicc, archivo)
+                archivo.close()
+                return True
+        except FileNotFoundError:
+            return False
