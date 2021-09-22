e79e93e72bb6b7001251d713b80c87cc5298a778
EnzoC <enzonicolasconti@gmial.com>
2021-09-22 15:49:08 -0300

    manejo de archivo

--- actions/actions.py
+++ actions/actions.py
@@ -6,22 +6,51 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+import json
+import os
+from os import name
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+#
+#
+class ActionHelloWorld(Action):
+#
+     def name(self) -> Text:
+         return "action_hello_world"
 
-# from typing import Any, Text, Dict, List
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+        
+        return []
+
+
+class ActionInfoEmpledo(Action):
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+     def name(self) -> Text:
+         return "action_info_empleado"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            direccionArch= os.getcwd() +"\\recursos\\empleados\\" + next(tracker.get_latest_entity_values("idEmpleado"), None) + ".json"
+            dicc= leerArchivo(direccionArch)
+            dispatcher.utter_message(dicc["nombre"])
+        
+            return []
+
+
+def leerArchivo(dire)-> dict:
+        with open (dire, "r") as archivo:
+            dict= json.load(archivo)
+            archivo.close()
+        return dict
+
+def escribirArchivo(dire, dicc): 
+        with open(dire, "w") as archivo:
+            json.dump(dicc, archivo)
+            archivo.close()
